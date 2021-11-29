5399b9a0dbd2f5d0a5ce53416fa469d2df877031
RobbinGIT <robbin.githimbo@student.moringaschool.com>
2021-11-29 21:15:56 +0300

    deploy to heroku

--- gallery/settings.py
+++ gallery/settings.py
@@ -115,7 +115,7 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = [
-    'gall101.herokuapp.com',
+    'robsgallery.herokuapp.com',
     '127.0.0.1'
 ]
 
